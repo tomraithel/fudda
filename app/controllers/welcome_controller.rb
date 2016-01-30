b800bdd5b8af7cb89156bd0bf8ea75abebf52129
Tom Raithel <t.raithel@googlemail.com>
2016-01-30 13:37:08 +0100

    Fixed style for today in welcome view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
 
     calendar = TickTack::Calendar.new(nil, {dow: 1});
+    @today_date = DateTime.now.to_date
+
     if params[:day]
       d = Date.parse(params[:day])
       @day = calendar.day(d.year, d.month, d.day)
