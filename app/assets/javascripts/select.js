a5d8a3486690f14ce70b43cfcaa588e5efef04ae
Tom Raithel <t.raithel@googlemail.com>
2016-02-09 21:30:18 +0100

    Improved select2 usability

--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -1,7 +1,18 @@
 (function() {
   var ready = function() {
     $('select').select2({
-      allowClear: true
+      allowClear: false
+    }).each(function() {
+      var $sel = $(this);
+      var $container = $sel.data('select2').$container.find('[tabindex]').eq(0);
+      $container.on('focus', function() {
+        $sel.select2("open");
+      });
+      $container.on('keydown', function(e) {
+        if(e.which === 13) {
+          $container.parents('form').submit();
+        }
+      });
     });
   };
 
